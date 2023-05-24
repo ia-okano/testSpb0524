39ec9e0923cd25928e99885dbb367320d553a3f3
N012-SV8-7\j_okano <j_okano@internetacademy.co.jp>
2023-05-24 21:37:07 +0900

    すべてプッシュ

---
+++ src/main/java/com/example/demo/TestSpb202211Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestSpb202211Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestSpb202211Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demo/TestSpb202211ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestSpb202211ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
